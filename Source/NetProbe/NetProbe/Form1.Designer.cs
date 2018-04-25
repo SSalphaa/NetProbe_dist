4040cf7536a055a25dcc2c256ec622661d9a450e
SAGNA Seckou-amadou-alpha <seckou-amadou-alpha.sagna@alstomgroup.com>
2018-04-25 18:22:55 +0200

    Sprint1/Day2: Capturing Packets from communicating interfaces

--- Source/NetProbe/NetProbe/Form1.Designer.cs
+++ Source/NetProbe/NetProbe/Form1.Designer.cs
@@ -28,21 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.nodeSelect = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 462);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(120, 40);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(776, 430);
+            this.treeView1.TabIndex = 1;
+            // 
+            // nodeSelect
+            // 
+            this.nodeSelect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.nodeSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.nodeSelect.FormattingEnabled = true;
+            this.nodeSelect.Location = new System.Drawing.Point(429, 476);
+            this.nodeSelect.Name = "nodeSelect";
+            this.nodeSelect.Size = new System.Drawing.Size(358, 24);
+            this.nodeSelect.TabIndex = 2;
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.nodeSelect);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnConnect);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
-            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.Activated += new System.EventHandler(this.Dashboard_Activate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Dashboard_FormClosing);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ComboBox nodeSelect;
     }
 }
 
--- Source/NetProbe/NetProbe/Form1.cs
+++ Source/NetProbe/NetProbe/Form1.cs
@@ -3,23 +3,165 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
 
 namespace NetProbe
 {
+    public enum Protocol
+    {
+        TCP = 6,
+        UDP = 17,
+        Unknown = -1
+    }
+
     public partial class Dashboard : Form
     {
+        private Socket mainSocket;                          //The socket which captures all incoming packets
+        private byte[] byteData = new byte[4096];
+        private bool bContinueCapturing = false;            //A flag to check if packets are to be captured or not
+        private int j = 0;
+
+        private delegate void AddTreeNode(TreeNode node);
+
         public Dashboard()
         {
             InitializeComponent();
         }
-
-        private void Dashboard_Load(object sender, EventArgs e)
+        private void btnConnect_Click(object sender, EventArgs e)
         {
+            if(nodeSelect.Text == "")
+            {
+                MessageBox.Show("Select an Interface to capture the packets.", "NetProbe",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                if (!bContinueCapturing)
+                {
+                    //Start capturing the packets...
+
+                    btnConnect.Text = "Disconnect";
+
+                    bContinueCapturing = true;
+
+                    //For sniffing the socket to capture the packets has to be a raw socket, with the
+                    //address family being of type internetwork, and protocol being IP
+                    mainSocket = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Raw, ProtocolType.IP);
+
+                    //Bind the socket to the selected IP address
+                    mainSocket.Bind(new IPEndPoint(IPAddress.Parse(nodeSelect.Text), 0));
+
+                    //Set the socket  options
+                    mainSocket.SetSocketOption(SocketOptionLevel.IP,            //Applies only to IP packets
+                                               SocketOptionName.HeaderIncluded, //Set the include the header
+                                               true);                           //option to true
+
+                    byte[] byTrue = new byte[4] { 1, 0, 0, 0 };
+                    byte[] byOut = new byte[4] { 1, 0, 0, 0 }; //Capture outgoing packets
+
+                    //Socket.IOControl is analogous to the WSAIoctl method of Winsock 2
+                    mainSocket.IOControl(IOControlCode.ReceiveAll,              //Equivalent to SIO_RCVALL constant
+                                                                                //of Winsock 2
+                                         byTrue,
+                                         byOut);
+
+                    //Start receiving the packets asynchronously
+                    mainSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None,
+                        new AsyncCallback(OnReceive), null);
+                }
+                else
+                {
+                    btnConnect.Text = "Connect";
+                    bContinueCapturing = false;
+                    //To stop capturing the packets close the socket
+                    mainSocket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "NetProbe", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void OnReceive(IAsyncResult ar)
+        {
+            try
+            {
+                int nReceived = mainSocket.EndReceive(ar);
+
+                //Analyze the bytes received...
+                //ParseData(byteData, nReceived);
+                showData(byteData);
+                if (bContinueCapturing)
+                {
+                    byteData = new byte[4096];
+
+                    //Another call to BeginReceive so that we continue to receive the incoming
+                    //packets
+                    mainSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None,
+                        new AsyncCallback(OnReceive), null);
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "NetProbe", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private string showData(byte[] frame)
+        {
+            TreeNode rootNode = new TreeNode();
+            String Data = null;
+            AddTreeNode addTreeNode = new AddTreeNode(OnAddTreeNode);
+
+            rootNode.Text = Convert.ToString(j++);
+            
+            for (int i = 0; i < frame.Length; i += 1)
+            {
+                Data += frame[i];
+            }
+            rootNode.Nodes.Add(Data);
+
+            //Thread safe adding of the nodes
+            treeView1.Invoke(addTreeNode, new object[] { rootNode });
+            return Data;
 
         }
+
+        private void OnAddTreeNode(TreeNode node)
+        {
+            treeView1.Nodes.Add(node);
+        }
+
+        private void Dashboard_Activate(object sender, EventArgs e)
+        {
+            string strIP = null;
+
+            IPHostEntry HosyEntry = Dns.GetHostEntry((Dns.GetHostName()));
+            if (HosyEntry.AddressList.Length > 0)
+            {
+                foreach (IPAddress ip in HosyEntry.AddressList)
+                {
+                    strIP = ip.ToString();
+                    nodeSelect.Items.Add(strIP);
+                }
+            }
+            nodeSelect.Items.Add("127.0.0.1");
+        }
+
+        private void Dashboard_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (bContinueCapturing)
+            {
+                mainSocket.Close();
+            }
+        }
+
     }
 }
